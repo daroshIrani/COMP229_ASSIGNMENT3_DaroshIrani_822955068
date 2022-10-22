364c767709e5cfefe9af57e09b67295c572a3cc4
daroshIrani <darosh29@gmail.com>
2022-10-22 02:59:55 -0400

    Created authorization controllers and routes and user schema or db

---
+++ app/models/user.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import passportLocalMongoose from 'passport-local-mongoose';
+const { PassportLocalSchema } = mongoose;
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: String,
+    emailAddress: String
+},{
+    timestamps:true,
+    collection: 'users'
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+export default mongoose.model('User', UserSchema);
