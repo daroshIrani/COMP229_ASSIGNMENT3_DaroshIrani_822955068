f8916af69e742ba45b7684d30e33e23d17d2c081
daroshIrani <darosh29@gmail.com>
2022-10-22 00:36:38 -0400

    Routes for Contact list are completed

--- app/controllers/contacts.controller.server.js
+++ app/controllers/contacts.controller.server.js
@@ -1,20 +1,85 @@
-import contacts from "../models/contacts.js";
+import contactModel from "../models/contacts.js";
 
 
 
 
 export function DisplayContactList(req,res,next){                                                 // We check if we have a list of things or an error - the function .find asks if 
-    contacts.find(function(err, contactCollection){                                            // the function has an error what to do and if it is successfull what to do
+    contactModel.find(function(err, contactCollection){                                            // the function has an error what to do and if it is successfull what to do
         if (err){                                                                               // we define what to do if there is an error - we do  error checking  
                                                                                                 // because we are now making a db connection with a controller
             console.error(err);
             res.end(err)
         }
-        res.render('index', {title:'Contacts', page:'contacts/contactCollection', contacts:contactCollection , displayName: UserDisplayName(req)} ); // if there is no error render the index page using the same header and footer as index,
-                                                                                                // but, page we render is under content/movies/list - title is given as will be filled in
-                                                                                                // the index.ejs page and the page it will be directed to is movies/list (follows 
-                                                                                                // same pattern as index.controller.server.js) - only difference is that one more variable
-                                                                                                // will be rendered which is the movies variable : as the movies collection list - this is 
-                                                                                                // used in the /content/movies/list page to generate the rows <% - .....%>
+        res.render('index', {title:'Contacts', page:'contacts/contactCollection', contact:contactCollection  } ); //displayName: UserDisplayName(req)// 
         })
     }
+
+export function DisplayContactAddPage (req,res, next){
+    res.render('index', {title: 'Add Contact', page : '/contact', contact:{}  }); // , displayName: UserDisplayName(req)
+}
+
+export function ProcessContactAddPage(req, res, next){
+    
+    let newContact = contactModel({
+        contactFirstName: req.body.fName,
+        contactLastName: req.body.lName,
+        contactNumber: req.body.contactNumber,
+        contactEmailAddress: req.body.contactEmail,
+    });
+
+    contactModel.create(newContact, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/contact-list')
+    } )
+}
+
+export function DisplayContactsUpdatePage(req, res, next){
+    let id = req.params.id;
+
+    contactModel.findById(id, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', { title: 'Update Contact', page: '/contact', contact: Contact}); //, displayName: UserDisplayName(req) 
+    });    
+}
+
+export function ProcessContactUpdatePage(req, res, next){
+
+    let id = req.params.id;   
+    let newContact = contactModel({
+        _id: req.body.id,
+        contactFirstName: req.body.fName,
+        contactLastName: req.body.lName,
+        contactNumber: req.body.contactNumber,
+        contactEmailAddress: req.body.contactEmail,
+    });
+
+    contactModel.updateOne({_id: id }, newContact, (err, Contact) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/contact-list')
+    } )
+}
+
+export function ProcessContactDelete(req, res, next){
+    let id = req.params.id;
+
+    contactModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/contact-list');
+    })
+}
--- app/routes/contacts.route.server.js
+++ app/routes/contacts.route.server.js
@@ -1,17 +1,16 @@
 import { Router } from "express";
 
-import { DisplayContactList } from "../controllers/contacts.controller.server.js";
+import { DisplayContactList, DisplayContactAddPage, ProcessContactAddPage, ProcessContactUpdatePage, DisplayContactsUpdatePage, ProcessContactDelete } from "../controllers/contacts.controller.server.js";
 
 const contactRouter = Router();
 
 
-contactRouter.get('/contact-list', DisplayContactList); 
-
-
-contactRouter.get('/contact-add', AuthGuard, DisplayMoviesAddPage);      
-contactRouter.post('/movie-add', AuthGuard, ProcessMoviesAddPage);
-contactRouter.post('/movie-edit/:id', AuthGuard, ProcessMoviesEditPage);
-contactRouter.get('/movie-edit/:id', AuthGuard, DisplayMoviesEditPage);
-contactRouter.get('/movie-delete/:id', AuthGuard, ProcessMoviesDelete)
+contactRouter.get('/contact-list', DisplayContactList);
+contactRouter.post('/contact-list', DisplayContactList);  
+contactRouter.get('/contact-add', DisplayContactAddPage);      
+contactRouter.post('/contact-add', ProcessContactAddPage);
+contactRouter.post('/contact-update/:id',  ProcessContactUpdatePage);
+contactRouter.get('/contact-update/:id', DisplayContactsUpdatePage);
+contactRouter.get('/contact-delete/:id',  ProcessContactDelete)
 
 export default contactRouter;
